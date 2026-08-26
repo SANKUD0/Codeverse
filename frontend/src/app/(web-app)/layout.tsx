4c93a2914e5f03dde2b719435f4e4cd89aa9b8cd
Antoine Fauche-Salman <tacofauche@gmail.com>
2026-08-25 20:40:07 -0400

    feat: add Layout component to wrap children in DashboardLayout

---
+++ frontend/src/app/(web-app)/layout.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "@/components/DasboardLayout";
+
+export default function Layout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <DashboardLayout>{children}</DashboardLayout>
+    );
+}
